85e3d6048e25761d3d0eec43379be45322cd0e11
ricardoharris12 <99985262+ricardoharris12@users.noreply.github.com>
2022-03-06 09:09:01 -0800

    added spacing and semicolons

--- scripts.js
+++ scripts.js
@@ -1,10 +1,22 @@
-const arrow = document.querySelector('.arrow');
-const speed = document.querySelector('.speed-value');
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.appendChild(highlight);
 
-navigator.geolocation.watchPosition((data) => {
-  console.log(data);
-  speed.textContent = data.coords.speed;
-  arrow.style.transform = `rotate(${data.coords.heading}deg)`;
-}, (err) => {
-  console.error(err);
-});
+function highlightLink() {
+  const linkCoords = this.getBoundingClientRect();
+  console.log(linkCoords);
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX
+  };
+
+  highlight.style.width = `${coords.width}px`;
+  highlight.style.height = `${coords.height}px`;
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
