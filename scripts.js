f944cc1a2d5be188463bbc1b1aa3620dbbf90e61
ricardoharris12 <99985262+ricardoharris12@users.noreply.github.com>
2022-03-06 09:12:28 -0800

    Update style.css

--- scripts.js
+++ scripts.js
@@ -1,22 +1,39 @@
-const triggers = document.querySelectorAll('a');
-const highlight = document.createElement('span');
-highlight.classList.add('highlight');
-document.body.appendChild(highlight);
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
+msg.text = document.querySelector('[name="text"]').value;
 
-function highlightLink() {
-  const linkCoords = this.getBoundingClientRect();
-  console.log(linkCoords);
-  const coords = {
-    width: linkCoords.width,
-    height: linkCoords.height,
-    top: linkCoords.top + window.scrollY,
-    left: linkCoords.left + window.scrollX
-  };
-
-  highlight.style.width = `${coords.width}px`;
-  highlight.style.height = `${coords.height}px`;
-  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
-
+function populateVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+    .filter(voice => voice.lang.includes('en'))
+    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+    .join('');
 }
 
-triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name === this.value);
+  toggle();
+}
+
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if (startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+function setOption() {
+  console.log(this.name, this.value);
+  msg[this.name] = this.value;
+  toggle();
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', () => toggle(false));
