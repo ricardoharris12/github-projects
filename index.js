e1f4bc446c61d19a1fe5b46cf0d83607cf86087e
ricardoharris12 <99985262+ricardoharris12@users.noreply.github.com>
2022-03-07 04:29:38 -0800

    Update style.css

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
-const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
+msg.text = document.querySelector('[name="text"]').value;
 
-const seconds = timeNodes
-  .map(node => node.dataset.time)
-  .map(timeCode => {
-    const [mins, secs] = timeCode.split(':').map(parseFloat);
-    return (mins * 60) + secs;
-  })
-  .reduce((total, vidSeconds) => total + vidSeconds);
+function populateVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+    .filter(voice => voice.lang.includes('en'))
+    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+    .join('');
+}
 
-  let secondsLeft = seconds;
-  const hours = Math.floor(secondsLeft / 3600);
-  secondsLeft = secondsLeft % 3600;
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name === this.value);
+  toggle();
+}
 
-  const mins = Math.floor(secondsLeft / 60);
-  secondsLeft = secondsLeft % 60;
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if (startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
 
-  console.log(hours, mins, secondsLeft);
+function setOption() {
+  console.log(this.name, this.value);
+  msg[this.name] = this.value;
+  toggle();
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', () => toggle(false));
