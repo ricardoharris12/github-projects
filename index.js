f01626748cde936f2ac558d97e2c3fb67b0d9608
ricardoharris12 <99985262+ricardoharris12@users.noreply.github.com>
2022-03-07 04:33:45 -0800

    Changed broken header background image to suitable replacement from u…

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
-const msg = new SpeechSynthesisUtterance();
-let voices = [];
-const voicesDropdown = document.querySelector('[name="voice"]');
-const options = document.querySelectorAll('[type="range"], [name="text"]');
-const speakButton = document.querySelector('#speak');
-const stopButton = document.querySelector('#stop');
-msg.text = document.querySelector('[name="text"]').value;
+   // Not a ton of code, but hard to
+   const nav = document.querySelector('#main');
+   let topOfNav = nav.offsetTop;
 
-function populateVoices() {
-  voices = this.getVoices();
-  voicesDropdown.innerHTML = voices
-    .filter(voice => voice.lang.includes('en'))
-    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
-    .join('');
-}
+   function fixNav() {
+     if (window.scrollY >= topOfNav) {
+       document.body.style.paddingTop = nav.offsetHeight + 'px';
+       document.body.classList.add('fixed-nav');
+     } else {
+       document.body.classList.remove('fixed-nav');
+       document.body.style.paddingTop = 0;
+     }
+   }
 
-function setVoice() {
-  msg.voice = voices.find(voice => voice.name === this.value);
-  toggle();
-}
-
-function toggle(startOver = true) {
-  speechSynthesis.cancel();
-  if (startOver) {
-    speechSynthesis.speak(msg);
-  }
-}
-
-function setOption() {
-  console.log(this.name, this.value);
-  msg[this.name] = this.value;
-  toggle();
-}
-
-speechSynthesis.addEventListener('voiceschanged', populateVoices);
-voicesDropdown.addEventListener('change', setVoice);
-options.forEach(option => option.addEventListener('change', setOption));
-speakButton.addEventListener('click', toggle);
-stopButton.addEventListener('click', () => toggle(false));
+   window.addEventListener('scroll', fixNav);
