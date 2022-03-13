bcf0f73e853d0fec3a9eedbe460c0fa162ab1ed3
ricardoharris12 <99985262+ricardoharris12@users.noreply.github.com>
2022-03-13 13:56:02 -0700

    user alert!

--- index.js
+++ index.js
@@ -1,48 +1,9 @@
-const holes = document.querySelectorAll('.hole');
-const scoreBoard = document.querySelector('.score');
-const moles = document.querySelectorAll('.mole');
-let lastHole;
-let timeUp = false;
-let score = 0;
+const name = /*unresolved*/ alert('Hi Welcome user');
+console.log('name');
 
-function randomTime(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
+const TransitionEvent = /*unresolved*/ StyleSheetList('button');
+const TransitionEvent = StyleSheet('GamepadButton');
+class button{TransitionEvent}; 
+console.log('button');
 
-function randomHole(holes) {
-  const idx = Math.floor(Math.random() * holes.length);
-  const hole = holes[idx];
-  if (hole === lastHole) {
-    console.log('Ah nah thats the same one bud');
-    return randomHole(holes);
-  }
-  lastHole = hole;
-  return hole;
-}
-
-function peep() {
-  const time = randomTime(200, 1000);
-  const hole = randomHole(holes);
-  hole.classList.add('up');
-  setTimeout(() => {
-    hole.classList.remove('up');
-    if (!timeUp) peep();
-  }, time);
-}
-
-function startGame() {
-  scoreBoard.textContent = 0;
-  timeUp = false;
-  score = 0;
-  peep();
-  setTimeout(() => timeUp = true, 10000)
-}
-
-function bonk(e) {
-  if(!e.isTrusted) return; // cheater!
-  score++;
-  this.parentNode.classList.remove('up');
-  scoreBoard.textContent = score;
-}
-
-moles.forEach(mole => mole.addEventListener('click', bonk));
+const addEventListener = ('true');
