7d5ef952eddc5d1cc239c0af74e8f8d8165d7df3
ricardoharris12 <99985262+ricardoharris12@users.noreply.github.com>
2022-03-13 14:09:06 -0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const name = /*unresolved*/ alert('Hi Welcome user');
-console.log('name');
-
-const TransitionEvent = /*unresolved*/ StyleSheetList('button');
-const TransitionEvent = StyleSheet('GamepadButton');
-class button{TransitionEvent}; 
-console.log('button');
-
-const addEventListener = ('true');
